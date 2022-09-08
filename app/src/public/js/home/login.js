96951dabe5b333c9f8a6dde53d57b3439fb3870b
MYNAMEISSHINJAESEOK <shinjaeseok7@gmail.com>
2022-09-08 16:23:33 +1000

    돔을이용하여 HTML

--- app/src/public/js/home/login.js
+++ app/src/public/js/home/login.js
@@ -1,2 +1,18 @@
-console.log("hello");
-console.log("hello");
+const id = document.querySelector("#id"),
+    psword = document.querySelector("#password"),
+    loginBtn = document.querySelector("button");
+
+loginBtn.addEventListener('click', () => {
+    login()
+});
+
+const login = () => {
+    const req = {
+        id: id.value,
+        psword: psword.value,
+    };
+
+    // fetch();
+
+    console.log(req);
+}
