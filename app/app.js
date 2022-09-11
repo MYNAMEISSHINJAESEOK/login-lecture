83a8100340f73a4b51d2ea51f30e0c5585a36e88
MYNAMEISSHINJAESEOK <shinjaeseok7@gmail.com>
2022-09-11 17:19:54 +1000

    morgan

--- app/app.js
+++ app/app.js
@@ -1,7 +1,15 @@
 const express = require('express');
 const app = express();
+const morgan = require("morgan");
 const dotenv = require("dotenv");
+const fs = require("fs");
+
+dotenv.config();
+
+
+const accessLogStream = require("./src/config/log")
 const home = require("./src/routes/home")
+
 
 app.set("views", "./src/views");
 app.set("view engine", "ejs");
@@ -11,6 +19,7 @@ app.use(express.static(`${__dirname}/src/public`));
 app.use(express.json());
 app.use(express.urlencoded({extended:true}));
 
+app.use(morgan("dev", { stream : accessLogStream}));
 app.use("/", home);
 
 module.exports = app;
